d311784815b7db18b7fbab56d0d010d9e5d9fed0
Anna Lindemann <anshalneva@gmail.com>
2025-10-01 12:51:35 +0200

    ci: add minimal workflow

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'scope-empty': [2, 'always'],
     'type-enum': [2, 'always', ['feat','fix','docs','refactor','test','ci','chore']],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
+    'header-max-length': [2, 'always', 90]
   }
 };
 
