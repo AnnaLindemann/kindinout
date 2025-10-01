5bbc882a65b2c6930c22a871495dc01298f209ea
Anna Lindemann <anshalneva@gmail.com>
2025-10-01 13:39:05 +0200

    fix: format files with prettier

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,11 +1,10 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'scope-empty': [2, 'always'],
-    'type-enum': [2, 'always', ['feat','fix','docs','refactor','test','ci','chore']],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 90]
-  }
+    "scope-empty": [2, "always"],
+    "type-enum": [2, "always", ["feat", "fix", "docs", "refactor", "test", "ci", "chore"]],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 90],
+  },
 };
-
